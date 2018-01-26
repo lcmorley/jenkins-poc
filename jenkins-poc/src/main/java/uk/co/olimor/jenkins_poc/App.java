8a7387ffea51029ceb1e4442f36f236b62caee21
Developer Environment <leon.morley@finkit.com>
2018-01-26 14:06:54 +0000

    initial check in

---
+++ jenkins-poc/src/main/java/uk/co/olimor/jenkins_poc/App.java
@@ -0,0 +1,13 @@
+package uk.co.olimor.jenkins_poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
